8255d5ff3529016c17eef7f6eb93443d7c28cfee
Fenil chauhan <91710798+Fenil2511@users.noreply.github.com>
2022-10-20 14:10:49 +0530

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
-
+const sidebar2 = document.querySelector('.sidebar');
 hamburger.addEventListener('click',  ()=>{
     hamburger.classList.toggle('clicked');
     sidebar.classList.toggle('show');
