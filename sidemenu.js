2f93e4c713bc278ecf4844c15ec1bd77a7074280
Idoitic-boi <116479686+Idoitic-boi@users.noreply.github.com>
2022-10-23 11:56:31 +0530

    Update sidemenu.js

--- sidemenu.js
+++ sidemenu.js
@@ -4,6 +4,7 @@
             document.getElementById("mySidenav").style.transition = "0.9s";
             document.getElementById("mySidenav").style.left = "0";
         }
+// Use comments
         // Function TO Close Nav
         function closeNav() {
           document.getElementById("mySidenav").style.transition = "2.0s";
